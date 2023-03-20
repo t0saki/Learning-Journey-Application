4956a2ae133b9ca8c7e185f24932dda700a8ea91
Zhengxiao Wu <wzx1459931670@outlook.com>
2023-03-20 22:48:43 +0800

    first ui

---
+++ LearningJourneyApp.java
@@ -0,0 +1,8 @@
+import UIDisplay.Login;
+
+public class LearningJourneyApp {
+    public static void main(String[] args) {
+        // Initialize the user interface
+        new Login();
+    }
+}
---
+++ UIDisplay/Login.java
@@ -0,0 +1,15 @@
+package UIDisplay;
+
+import javax.swing.*;
+
+public class Login {
+    // Just show a simple login screen
+    public Login() {
+        // Create a new JFrame
+        JFrame frame = new JFrame("Login");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300, 200);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+}
