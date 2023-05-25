ad2aa334b6357d3f61b9815b18e16f4b4637316f
Zhengxiao Wu <t0saki@outlook.com>
2023-05-25 20:06:43 +0800

    new courseschedule

--- Boundary/CourseSchedule.java
+++ Boundary/CourseSchedule.java
@@ -9,14 +9,13 @@ import java.util.Map;
 
 public class CourseSchedule extends JPanel {
 
-    private static final String[] DAYS_OF_WEEK = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday" };
-    private static final String[] TIME_SLOTS = { "Morning", "Late Morning", "Noon", "Afternoon", "Late Afternoon",
-            "Evening" };
+    private static final String[] DAYS_OF_WEEK = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday"};
+    private static final String[] TIME_SLOTS = {"Morning", "Late Morning", "Noon", "Afternoon", "Late Afternoon", "Evening"};
 
     private String studentID;
 
     public CourseSchedule(String studentID) {
-        setPreferredSize(new Dimension(680, 900));
+        setPreferredSize(new Dimension(600, 700));
         this.studentID = studentID;
         refresh();
     }
@@ -55,10 +54,11 @@ public class CourseSchedule extends JPanel {
         BaseHandler baseHandler = new BaseHandler();
         baseHandler.open("Data/Schedule/" + studentID + ".csv");
         String[][] schedule = baseHandler.getFileData();
+        // Course,Day,Time,Location,Professor
         baseHandler.close();
-        HashMap<String, String> scheduleMap = new HashMap<>();
+        HashMap<String, Integer> scheduleMap = new HashMap<>();
         for (int i = 0; i < schedule.length; i++) {
-            scheduleMap.put(schedule[i][1] + " " + schedule[i][2], schedule[i][0]);
+            scheduleMap.put(schedule[i][1] + " " + schedule[i][2], i);
         }
 
         // Add time labels to the left column and empty cells to the rest
@@ -67,9 +67,23 @@ public class CourseSchedule extends JPanel {
             for (int j = 0; j < DAYS_OF_WEEK.length; j++) {
                 JPanel cellPanel = new JPanel();
                 cellPanel.setBorder(BorderFactory.createLineBorder(Color.GRAY));
+                cellPanel.setLayout(new GridLayout(3, 1));
                 if (scheduleMap.containsKey(DAYS_OF_WEEK[j] + " " + TIME_SLOTS[i])) {
-                    cellPanel.add(
-                            new JLabel(scheduleMap.get(DAYS_OF_WEEK[j] + " " + TIME_SLOTS[i]), SwingConstants.CENTER));
+                    // Bigger course name
+                    JLabel courseLabel = new JLabel(schedule[scheduleMap.get(DAYS_OF_WEEK[j] + " " + TIME_SLOTS[i])][0], SwingConstants.CENTER);
+                    JLabel locationLabel = new JLabel(schedule[scheduleMap.get(DAYS_OF_WEEK[j] + " " + TIME_SLOTS[i])][3], SwingConstants.CENTER);
+                    JLabel professorLabel = new JLabel(schedule[scheduleMap.get(DAYS_OF_WEEK[j] + " " + TIME_SLOTS[i])][4], SwingConstants.CENTER);
+
+
+                    courseLabel.setFont(courseLabel.getFont().deriveFont(Font.BOLD, 20f));
+                    // Smaller other info
+                    locationLabel.setFont(locationLabel.getFont().deriveFont(Font.PLAIN, 14f));
+                    professorLabel.setFont(professorLabel.getFont().deriveFont(Font.PLAIN, 14f));
+
+                    cellPanel.add(courseLabel);
+                    cellPanel.add(locationLabel);
+                    cellPanel.add(professorLabel);
+
                     cellPanel.setBackground(Color.GREEN);
                 }
                 add(cellPanel);
