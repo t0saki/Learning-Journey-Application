a0cc1c8adb26738b32c3ef640c8cc160996e88c7
ygyydds <908992763@qq.com>
2023-05-26 21:35:45 +0800

    change color

--- Entity/ModuleItem.java
+++ Entity/ModuleItem.java
@@ -27,27 +27,28 @@ public class ModuleItem extends ClickableItem {
         userInfoHandler.open("Data/Modules&Marks.csv");
 
         JLabel label1 = new JLabel();
-        // JLabel label2 = new JLabel();
+        JLabel label2 = new JLabel();
         moduleName = getModuleName(col);
         Mark = getModuleMark(studentID, moduleName);
         this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
         this.setPreferredSize(new Dimension(150, 100));
         // this.setSize(150, 100);
         if (Mark < 60) {
-            this.setBackground(Color.red);
+            label2.setText("<html><sup>Fail</sup></html>");
         } else if (Mark > 60 && Mark <= 70) {
-            this.setBackground(Color.orange);
+            label2.setText("<html><sup>Pass</sup></html>");
         } else if (Mark > 70 && Mark <= 80) {
-            this.setBackground(Color.yellow);
+            label2.setText("<html><sup>Good</sup></html>");
         } else if (Mark > 80 && Mark <= 90) {
-            this.setBackground(Color.CYAN);
+            label2.setText("<html><sup>Merit</sup></html>");
         } else {
-            this.setBackground(Color.GREEN);
+            label2.setText("<html><sup>Excellent</sup></html>");
+            //this.setBackground(Color.GREEN);
         }
 
         label1.setText("<html><center>" + moduleName + "<br>" + String.valueOf(Mark) + "</center></html>");
         label1.setHorizontalAlignment(SwingConstants.CENTER);
-        // label2.setText(String.valueOf(Mark));
+        //label2.setText("<html><sup>111</sup></html>");
         // setGradientWidth(Mark);
         // repaint();
         // Font font = new Font("Arial", Font.BOLD, 16);
@@ -63,11 +64,11 @@ public class ModuleItem extends ClickableItem {
         // fillPanel.setBackground(Color.BLUE);
         // fillPanel.setPreferredSize(new Dimension(fillWidth, getHeight() - 2 *
         // padding));
-
+        this.add(label2);
         this.add(Box.createVerticalGlue());
         this.add(label1);
         this.add(Box.createVerticalStrut(10));
-        // this.add(label2);
+
         // add(fillPanel);
         this.add(Box.createVerticalGlue());
         // label1.setSize( 150, 45);
@@ -105,22 +106,42 @@ public class ModuleItem extends ClickableItem {
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        // // Create a gradient paint
-        //// GradientPaint gradient = new GradientPaint(
-        //// 0, 0, Color.PINK, getWidth(), getHeight(), Color.orange);
-        // GradientPaint gradient = new GradientPaint(
-        // 0, 0, Color.BLUE, getWidth(), getHeight(), Color.CYAN);
-        // // Create a graphics2D object from the graphics object
-        // Graphics2D g2d = (Graphics2D) g;
-        //
-        // // Set the paint to be the gradient
-        // g2d.setPaint(gradient);
-        //
-        // // Draw a rectangle filled with the gradient
-        // g2d.fillRect(0, 0, getWidth(), getHeight());
+        int width = getWidth();
+        int height = getHeight();
 
-        // Draw a border around the panel
-        g.setColor(Color.BLACK);
+        if (Mark < 60) {
+            //label2.setText("<html><sup>Fail</sup></html>");
+            GradientPaint gradient = new GradientPaint(0, 0, new Color(255, 158, 158), width, height, new Color(255, 192, 144));
+            ((Graphics2D) g).setPaint(gradient);
+        } else if (Mark > 60 && Mark <= 70) {
+            //label2.setText("<html><sup>Pass</sup></html>");
+            GradientPaint gradient = new GradientPaint(0, 0, new Color(250, 202, 190), width, height, new Color(247, 226, 180));
+            ((Graphics2D) g).setPaint(gradient);
+        } else if (Mark > 70 && Mark <= 90) {
+            //label2.setText("<html><sup>Good</sup></html>");
+            GradientPaint gradient = new GradientPaint(0, 0, new Color(177, 215, 180), width, height, new Color(248, 249, 136));
+            ((Graphics2D) g).setPaint(gradient);
+        } else {
+            //label2.setText("<html><sup>Excellent</sup></html>");
+            GradientPaint gradient = new GradientPaint(0, 0, new Color(127, 183, 126), width, height, new Color(192, 238, 228));
+            ((Graphics2D) g).setPaint(gradient);
+        }
+         // Create a gradient paint
+        // GradientPaint gradient = new GradientPaint(
+        // 0, 0, Color.PINK, getWidth(), getHeight(), Color.orange);
+//         GradientPaint gradient = new GradientPaint(
+//         0, 0, Color.CYAN, getWidth(), getHeight(), Color.BLUE);
+//         // Create a graphics2D object from the graphics object
+//         Graphics2D g2d = (Graphics2D) g;
+//
+//         // Set the paint to be the gradient
+//         g2d.setPaint(gradient);
+//
+         // Draw a rectangle filled with the gradient
+         g.fillRect(0, 0, getWidth(), getHeight());
+
+         //Draw a border around the panel
+        g.setColor(Color.WHITE);
         g.drawRect(0, 0, getWidth() - 1, getHeight() - 1);
     }
 
