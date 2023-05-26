92c9eac49d4046f171d4d4b2c92192e7d4a47420
Zhengxiao Wu <t0saki@outlook.com>
2023-05-26 21:54:40 +0800

    histogram change

--- Boundary/Histogram.java
+++ Boundary/Histogram.java
@@ -7,7 +7,7 @@ import java.util.Arrays;
 /**
  * @author Zhengxiao Wu
  * @date 2023/05/25
- *     the Histogram panel, used to display the histogram of a student's marks
+ * the Histogram panel, used to display the histogram of a student's marks
  */
 
 public class Histogram extends JPanel {
@@ -33,20 +33,41 @@ public class Histogram extends JPanel {
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        int MaxMark = getMax();
-        int MinMark = getMin();
-        int MarkStep = (MaxMark - MinMark) / step;
-        int barWidth = getWidth() / (MarkStep + 1) - 10;
+        int MaxMark = 100;
+        int MinMark = 60;
+        int MarkStep = 5;
+        int barWidth = getWidth() / (MarkStep) - 10;
 
-        int[] frequency = new int[MarkStep + 1];
+        int[] frequency = new int[MarkStep];
+        // Below 60, 60-70, 70-80, 80-90, 90-100
         for (int i = 0; i < data.length; i++) {
-            int index = (data[i] - MinMark) / step;
-            frequency[index]++;
+            if (data[i] < 60) {
+                frequency[0]++;
+            } else if (data[i] < 70) {
+                frequency[1]++;
+            } else if (data[i] < 80) {
+                frequency[2]++;
+            } else if (data[i] < 90) {
+                frequency[3]++;
+            } else {
+                frequency[4]++;
+            }
         }
-        MarkStep++;
+
+        // Color from red to green
+        Color[] colors = {
+                new Color(255, 0, 0),
+                new Color(255, 128, 0),
+                new Color(255, 255, 0),
+                new Color(128, 255, 0),
+                new Color(0, 255, 0)
+        };
 
         // 绘制直方图的每个条形
         for (int i = 0; i < MarkStep; i++) {
+            // Color
+            g.setColor(colors[i]);
+
             int x = i * (barWidth + 10) + 10;
             int barHeight = (int) (((double) frequency[i] / Arrays.stream(frequency).max().getAsInt() * getHeight()) * 0.9);
 
@@ -59,8 +80,9 @@ public class Histogram extends JPanel {
         }
 
         // 绘制X轴和Y轴
+        g.setColor(Color.BLACK);
         g.drawLine(0, getHeight(), getWidth(), getHeight()); // X轴
-        g.drawLine(0, 0, 0, getHeight()); // Y轴
+//        g.drawLine(0, 0, 0, getHeight()); // Y轴
     }
 
     private int getMax() {
